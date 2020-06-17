d5262270fa182f903231acc11af7d38de2e09960
Pedro Fellipe Mendes Amaral - Analista de Sistemas <pedro.amaral@unievangelica.edu.br>
2020-06-17 16:52:31 -0300

    card model

--- models/user-model.js
+++ models/user-model.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 const { ObjectId } = mongoose.Schema;
+const moment = require('moment');
+
 
 const userModel = new schema({
     nome: {
@@ -16,6 +18,10 @@ const userModel = new schema({
     senha: {
         type:String,
     },
+    payDay:{
+        type:Date,
+        default:Date.now()
+    },
     ativo: {
         type:Boolean,
         required: true,
@@ -28,10 +34,10 @@ const userModel = new schema({
 },{versionKey: false});
 
 userModel.pre('save', (next) => {
-    const agora = new Date();
-
+    let agora = new Date();
+    const datav = new Date(moment().add(7,"days").d.toISOString());
     if (!this.createdAt) this.createdAt = agora;
-
+    if(!this.payDay) this.createdAt = datav;
     next();
 });
 
